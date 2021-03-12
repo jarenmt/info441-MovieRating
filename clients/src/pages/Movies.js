4bd0efc9caa9693511968bc5f6b1d4fc60333f13
Jaren Tilley <jarentilley@MacBook-Pro-2.local>
2021-03-11 23:16:12 -0800

    change movies

--- clients/src/pages/Movies.js
+++ clients/src/pages/Movies.js
@@ -1,61 +1,111 @@
-import React, { Component } from 'react';
+import React from "react";
 
 function Movies() {
-    return (
-        <div className="moviePage">
-            <h1>Top Movies</h1>
-            <div className="row">
-                <div className="column">
-                    <div className="card">
-                        <h3>Movie 1</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <h3>Movie 2</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <h3>Movie 3</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <h3>Movie 4</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <h3>Movie 5</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
-                </div>
-            </div>
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  return (
+    <div className="moviePage">
+      <h1>Top Movies</h1>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <h3>The Shawshank Redemption</h3>
+            <img
+              src={
+                "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+              }
+              alt="The Shawshank Redemption Movie Picture"
+            />
 
-            <br />
-
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-
-            <br />
-
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-
-            <br />
-
-            <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+            <p>Director: Frank Darabont</p>
+          </div>
         </div>
-    )
+        <div className="column">
+          <div className="card">
+            <h3>The Godfather</h3>
+            <img
+              src={
+                "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg"
+              }
+              alt="The Godfather Movie Picture"
+            />
+            <p>Director: Frank Ford Coppola</p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <h3>Pulp Fiction</h3>
+            <img
+              src={
+                "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR1,0,182,268_AL_.jpg"
+              }
+              alt="Pulp Fiction Movie Picture"
+            />
+            <p>Director: Quentin Tarantino</p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <h3>Fight Club</h3>
+            <img
+              src={
+                "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
+              }
+              alt="Fight Club Movie Picture"
+            />
+            <p>Director: David Fincher</p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="card">
+            <h3>The Dark Knight</h3>
+            <img
+              src={
+                "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+              }
+              alt="The Dark Knight Movie Picture"
+            />
+            <p>Director: Christopher Nolan</p>
+          </div>
+        </div>
+      </div>
+      <p>
+        Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
+        definitiones no quo, maluisset concludaturque et eum, altera fabulas ut
+        quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert
+        laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no
+        molestiae voluptatibus.
+      </p>
+
+      <br />
+
+      <p>
+        Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
+        definitiones no quo, maluisset concludaturque et eum, altera fabulas ut
+        quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert
+        laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no
+        molestiae voluptatibus.
+      </p>
+
+      <br />
+
+      <p>
+        Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
+        definitiones no quo, maluisset concludaturque et eum, altera fabulas ut
+        quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert
+        laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no
+        molestiae voluptatibus.
+      </p>
+
+      <br />
+
+      <p>
+        Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
+        definitiones no quo, maluisset concludaturque et eum, altera fabulas ut
+        quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert
+        laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no
+        molestiae voluptatibus.
+      </p>
+    </div>
+  );
 }
 
 export default Movies;
