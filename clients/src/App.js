2f28c0562b3be381f2fa4a7d1ec4fce535c21293
matthewjmun <56237565+matthewjmun@users.noreply.github.com>
2021-03-12 15:17:07 -0800

    Update App.js

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
-import "./App.css";
-import Login from "./pages/Login";
-import Movies from "./pages/Movies";
-import Registration from "./pages/Registration";
-import TV from "./pages/TV";
+import React, { Component } from 'react';
+import './App.css';
+import { Router, Switch, Route, Link, Redirect, NavLink, BrowserRouter } from 'react-router-dom';
+import Movies from './pages/Movies';
+import TV from './pages/TV';
+
 class App extends Component {
   render() {
     return (
@@ -12,10 +11,9 @@ class App extends Component {
         <BrowserRouter>
           <Nav />
           <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path="/" />
             <Route path="/movies" component={Movies} />
             <Route path="/tv" component={TV} />
-            <Route path="/registration" component={Registration} />
           </Switch>
         </BrowserRouter>
         <Footer />
@@ -28,14 +26,14 @@ class Nav extends Component {
   render() {
     return (
       <div class="nav">
-        <Link to="/">Moovio</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/tv">TV Shows</Link>
+        <Link to="/">Moovie</ Link>
+        <Link to="/movies">Movies</ Link>
+        <Link to="/tv">TV Shows</ Link>
         <div class="nav-right">
-          <Link to="/login">Login</Link>
+          <Link to="/login">Login</ Link>
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -43,9 +41,9 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>&copy; 2021 INFO 441</p>
-      </footer>
-    );
+        <p>&copy;  2021 INFO 441</p>
+      </footer >
+    )
   }
 }
 
